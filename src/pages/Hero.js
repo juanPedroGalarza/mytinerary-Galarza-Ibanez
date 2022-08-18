1b0c506c3b99b88ee2e74c728df132e519fef4cd
juanPedroGalarza <juanpg232@gmail.com>
2022-08-18 19:16:25 -0300

    Hero.js created

---
+++ src/pages/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../styles/Hero.css";
+const Hero = () => {
+  return (
+      <div className="Hero-main">
+        <div className='Hero-background'></div>
+          <h1 className='Hero-title'>MyTinerary</h1>
+          <div className='Hero-description'>
+            <div className='Hero-subtitle-container'>
+              <p className='Hero-subtitle-1'>Find your perfect trip,</p>
+              <p className='Hero-subtitle-2'>designed by insiders who knows and love their cities!</p>
+            </div>
+            <button type="button" className='Hero-button'>Let's go!</button>
+          </div>
+    </div>
+  )
+}
+
+export default Hero
