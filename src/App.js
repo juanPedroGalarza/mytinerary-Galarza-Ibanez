cebf76c3b545bc4ace3a9167d044f6687a7cf171
IbanezAndrea <mrs3lite@gmail.com>
2022-08-19 16:02:51 -0300

    error App.js areglado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Hero from './pages/Hero';
 import Footer from "./components/Footer";
-import Header from './components/Header';
+
 function App() {
   return (
     <>
-    <Header />
       <Hero />
-      <Footer />
     </>
   );
 }
