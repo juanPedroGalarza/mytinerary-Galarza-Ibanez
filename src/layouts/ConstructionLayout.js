28bbb4c6e0afe1ccea566a0d7e3b8cdc88cb506e
IbanezAndrea <mrs3lite@gmail.com>
2022-08-19 17:08:12 -0300

    underconstruction

---
+++ src/layouts/ConstructionLayout.js
@@ -0,0 +1,14 @@
+
+function ConstructionLayout(props) {
+
+    return (
+        <div>
+            {props.children}
+            <h2>
+                ⚙ Under Construction ⚙
+            </h2>
+        </div>
+    )
+}
+
+export default ConstructionLayout
