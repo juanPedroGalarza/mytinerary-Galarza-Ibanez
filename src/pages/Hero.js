c99a93b0059936e8ff112bf785533c48e509f02d
juanPedroGalarza <juanpg232@gmail.com>
2022-08-19 18:59:45 -0300

    CitiesCarousel to Hero page

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "../styles/Hero.css";
 import WebsiteLayout from '../layouts/WebsiteLayout';
-
+import CitiesCarousel from '../components/CitiesCarousel';
 const Hero = () => {
   return (
     <WebsiteLayout>
+      <>
       <div className="Hero-main">
         <div className='Hero-background'></div>
         <h1 className='Hero-title'>MyTinerary</h1>
@@ -16,6 +17,8 @@ const Hero = () => {
           <button type="button" className='Hero-button'>Let's go!</button>
         </div>
       </div>
+      <CitiesCarousel />
+      </>
     </WebsiteLayout>
   )
 }
