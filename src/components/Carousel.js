d3a285bb7885bdf07aff5797c3b5c6a83e48c847
juanPedroGalarza <juanpg232@gmail.com>
2022-08-22 14:56:59 -0300

    Carousel events

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,13 @@
 import "../styles/Carousel.css"
+import Arrow from "./Carousel/Arrow";
+import { useEffect, useState } from "react";
 function Carousel(props) {
     let items = props.data
     let range = props.range
-    let start = props.start - props.range
-    let end = start + range
-    const slidesCont = Math.ceil(items.length / 4)
+    let [start,setStart] = useState(props.start)
+    let [end,setEnd] = useState(start + range)
+    const interval = props.interval * 1000
+    const [intervalId,setIntervalId] = useState(null)
     const itemView = (item) => (
         <div className="Carousel-item">
             <p className="Carousel-item-title">{item.title}</p>
@@ -15,14 +18,37 @@ function Carousel(props) {
             <div className="Carousel-slide">
                 {slideItems.map(itemView)}
             </div>
-        )
+    )
+    function next() {
+        if (end <= items.length - range) {
+            setStart(start + range)
+            setEnd(end + range)
+            return
+        }
+        setStart(0)
+        setEnd(range)
+    }
+    function prev() {
+        if (start > 0) {
+            setStart(start - range)
+            setEnd(end - range)
+            return
+        }
+        setStart(items.length - range)
+        setEnd(items.length)
+    }
+    useEffect(() => {
+        let id = setInterval(next, interval)
+        setIntervalId(id)
+        return () => clearInterval(intervalId)
+    },[end])
     return (
         <div className="Carousel-container">
             <h3 className="Carousel-title">Popular MYtineraries</h3>
             <div className="Carousel">
-                {slideView(items.slice(start += range, end += range))}
-                {slideView(items.slice(start += range, end += range))}
-                {slideView(items.slice(start += range, end += range))}
+                <Arrow icon="<" click={ prev } />
+                {slideView(items.slice(start, end))}
+                <Arrow icon=">" click={ next }/>
             </div>
         </div>
  )   
