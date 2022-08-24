a7cfae37705d231d0d7d090e5b7a1d2525af597a
juanPedroGalarza <juanpg232@gmail.com>
2022-08-24 13:01:43 -0300

    nav modal responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
 import '../styles/Header.css'
-import {Link as LinkRouter} from 'react-router-dom'
-
-function Header() {
+import { Link as LinkRouter } from 'react-router-dom'
+import ProfileNavList from "../components/Profile/ProfileNavList"
+import { useEffect, useState } from 'react'
+import {useLocation} from "react-router-dom"
+function Header(props) {
+    const pages = props.data
+    const [open,setOpen] = useState(false)
+    const viewNav = (page) => {
+        return (<LinkRouter to={page.linkTo} className='Header-a'>{page.name}</LinkRouter>)
+    }
+    function toggleOpen() {
+        open?
+            setOpen(false)
+            :setOpen(true)
+    }
+    const location = useLocation()
+    useEffect(() => {
+        setOpen(false)
+    },[location.pathname])
     return (
         <div className='Header-nav-div'>
             <div className='Header-items-div'>
                 <img className='Header-logo' src='https://i.ibb.co/WfpYBpB/logomytinerary.png'></img>
                 <p className='Header-p'>MyTinerary</p>
             </div>
+            <span onClick={toggleOpen} className="Header-open-nav"></span>
             <nav className='Header-nav'>
-                <LinkRouter to='/' className='Header-a'>Home</LinkRouter>
-                <LinkRouter to='/cities' className='Header-a'>Cities</LinkRouter>
-                <LinkRouter to='/*' className='Header-a'>Log In</LinkRouter>
-                <LinkRouter to='/*' className='Header-a'>Sign Up</LinkRouter>
-                <img className='Header-user' src="https://i.ibb.co/jgp9dqj/user2.png"/>
+                {pages.map(viewNav)}
+                <ProfileNavList />
             </nav>
+            {open ?
+                <nav className='Header-nav-modal'>
+                <span onClick={toggleOpen} className="Header-close-nav"></span>
+                {pages.map(viewNav)}
+                <ProfileNavList />
+                </nav>
+                :null
+            }
+            
         </div>
     )
 }
