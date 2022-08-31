376e5ea99b8ae5e3ac1339bb5424ef8e39ab76f5
juanPedroGalarza <juanpg232@gmail.com>
2022-08-31 14:13:32 -0300

    inputArray NewCity

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,13 +1,51 @@
-import Input from "../components/NewCity/Input"
+import Input from "../components/Input"
 import "../styles/NewCity.css"
 
 
 function NewCity() {
+    const inputArray =[
+        {
+            name: "City",
+            type: "text",
+            placeholder: "type the name of the city!",
+            value: ""
+        },
+        {
+            name: "Country",
+            type: "text",
+            placeholder: "Type the contry where the city is from!",
+            value: ""
+        },
+        {
+            name: "Photo",
+            type: "url",
+            placeholder: "Insert the image url of the city!",
+            value: ""
+        },
+        {
+            name: "Population",
+            type: "number",
+            placeholder: "Type the population of the city!",
+            value: ""
+        },
+        {
+            name: "Fundation",
+            type: "date",
+            placeholder: "Type the date of fundation of the city!",
+            value: ""
+        },
+        {
+            name: "Description",
+            type: "text",
+            placeholder: "Tell us about the city!",
+            value: ""
+        }
+    ]
     return (
         <div className="NewCity-main">
             <div className="NewCity-background"></div>
             <h1 className="NewCity-title">NewCity</h1>
-            <Input />
+            <Input inputsData={inputArray} event={e=>console.log(e)} classPage="NewCity"/>
         </div>
     )   
 }
