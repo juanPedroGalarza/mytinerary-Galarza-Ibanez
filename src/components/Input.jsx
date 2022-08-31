bd5a5b0e5fe3b63ca32dd73986a402b438078f3c
juanPedroGalarza <juanpg232@gmail.com>
2022-08-31 14:11:01 -0300

    classNames and sendData input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,23 +3,26 @@ import { useRef } from "react"
 export default function Input(props) {
     const inputArray = props.inputsData
     const formEl = useRef()
-
+    const classPage = props.classPage
     const sendData = (e) => {
-        props.event(e,Array.from(formEl.current))
+        e.preventDefault()
+        props.event(Array.from(formEl.current),e)
+        formEl.current.reset()
     }
     const formStructure = (item) =>{
         return (
-            <label className="NewCity-form-label" key={item.name}>
-            <p className="NewCity-form-p">{item.name}</p>
-                <input className="NewCity-form-input" placeholder={item.placeholder} type={item.type} name={item.name} defaultValue={item.value} />
+            <label className={classPage + "-form-label"} key={item.name}>
+            <p className={classPage + "-form-p"}>{item.name}</p>
+                <input className={classPage + "-form-input"} placeholder={item.placeholder} type={item.type} name={item.name} defaultValue={item.value} />
             </label> 
         )
     }
     return (
         <>
-            <form className="NewCity-form" id="newcity-form" onSubmit={sendData} ref={formEl}>
+            <form className={classPage + "-form"} id="newcity-form" onSubmit={sendData} ref={formEl}>
+                {props.children}
                 {inputArray.map(formStructure)}
-                <button type="submit" className="NewCity-form-button">Send</button>
+                <button type="submit" className={classPage + "-form-button"}>Send</button>
             </form>
         </>
         )
