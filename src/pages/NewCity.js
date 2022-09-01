6f6f0d3a11db8eefd4ee2736ef97f8a70ba82669
IbanezAndrea <mrs3lite@gmail.com>
2022-09-01 03:02:24 -0300

    NewCity y styles

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,8 +1,9 @@
 import Input from "../components/Input"
 import "../styles/NewCity.css"
+import axios from "axios"
+import { useState } from "react";
 
-
-function NewCity() {
+function NewCity(props) {
     const inputArray =[
         {
             name: "City",
@@ -41,11 +42,26 @@ function NewCity() {
             value: ""
         }
     ]
+    const [valueSelect,setValueSelect] = useState(null)
+    const handleValue = (e) => {
+        setValueSelect(e.current.value)
+    }
+    const postCity = (arrayform) =>{
+        let inputsForm = arrayform
+        let data = inputsForm.reduce((values,input) => {
+            values[input.name.toLowerCase()] = input.value
+            return values
+        },{})
+        axios.post(`http://localhost:4000/cities` ,data)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className="NewCity-main">
-            <div className="NewCity-background"></div>
+            <div className="NewCity-background"> </div>
             <h1 className="NewCity-title">NewCity</h1>
-            <Input inputsData={inputArray} event={e=>console.log(e)} classPage="NewCity"/>
+            <Input inputsData={inputArray} event={(arrayForm)=>postCity(arrayForm)} onChange={handleValue}classPage="NewCity"/>
         </div>
     )   
 }
