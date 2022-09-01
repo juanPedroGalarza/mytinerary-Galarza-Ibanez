8369d93667503e61d0b97afebc658819de2e9a24
juanPedroGalarza <juanpg232@gmail.com>
2022-08-31 22:49:14 -0300

    Input Search cities

--- src/pages/CitiesPage.js
+++ src/pages/CitiesPage.js
@@ -1,21 +1,30 @@
 import TableList from "../components/TableList"
 import "../styles/CitiesPage.css"
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 function CitiesPage() {
-
-const [cities, setCities] = useState([])
-
-useEffect(()=> {
-        axios.get("http://localhost:4000/cities")
-        .then(res => setCities(res.data.response))
-},[])
-
+    const [cities, setCities] = useState([])
+    const [searchValue,setSearchValue] = useState("")
+    const handleValue = (e) => {
+        setSearchValue(e.target.value)
+    }
+    useEffect(() => {
+        if (searchValue) {
+            axios.get(`http://localhost:4000/cities/?city=${searchValue}`)
+            .then(res => setCities(res.data.response))
+            .catch(err => console.log(err))
+        } else {
+            axios.get("http://localhost:4000/cities")
+            .then(res => setCities(res.data.response))
+            .catch(err => console.log(err))
+        }
+    },[searchValue])
     return (
         <div className="CitiesPage-main">
             <h1 className="CitiesPage-title">Cities</h1>
+            <input type="search" placeholder="Search" name="city" onChange={handleValue} className="citiesPage-search" />
             <TableList data={cities} />
         </div>
     )
