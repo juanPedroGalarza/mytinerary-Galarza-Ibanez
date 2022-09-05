3b5a242f0ecce84f499c8c417ba2ead5e3347e25
juanPedroGalarza <juanpg232@gmail.com>
2022-09-05 01:11:00 -0300

    itineraries added to Details

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,36 +1,47 @@
 import "../styles/CityDetails.css"
 import { Link as LinkRouter } from 'react-router-dom'
-
+import axios from "axios"
+import Itinerary from "./Itinerary/Itinerary"
+import { useEffect } from "react"
+import { useState } from "react"
 function Details(props) {
     const city = props.data
     const id = props.cityId
-    const printCard = (item) => {
-
-        return (
-            <>
-                <div className="Details-item" style={{ backgroundImage: `url(${item.photo})` }}>
-                    <p className="Details-item-title">{item.city}</p>
-                </div>
-                <div className="Details-p-div">
-                    <div className="details-p-innerdiv">
-                    <p className="Details-innerp"><span className="details-p-span">Country:</span> {item.country}.</p>
-                        <p className="Details-innerp" ><span className="details-p-span">Fundation:</span> {item.fundation}.</p>
-                        <p className="Details-innerp" ><span className="details-p-span"> Population:</span> {item.population}.</p>
-                    </div>
-                        <p className="Details-p" >{item.description}</p>
-                        <h3 className="Details-subtitle"> We don't have any itineraries here right now...</h3>
-                </div>
-                <div className="Details-btns">
-                        <LinkRouter to={`/`} className="Details-btn">Back to Home</LinkRouter>
-                        <LinkRouter to={`/cities`}className="Details-btn">Back to Cities</LinkRouter>
-                        <LinkRouter to={`/edit-city/${id}`}className="Details-btn">Edit</LinkRouter>
-                </div>
-            </>
-        )
-    }
+    const [itineraries,setItineraries] = useState([])
+    useEffect(() => {
+        if (city) {
+            axios.get(`http://localhost:4000/itineraries/?city=${city.city}`)
+            .then(res => { setItineraries(res.data.response) })
+            .catch(err=>{
+                console.log(err)
+            })
+        }
+    },[city])
     return (
         <div className="Details-container">
-            {printCard(city)}
+            <div className="Details-item" style={{ backgroundImage: `url(${city.photo})` }}>
+                <p className="Details-item-title">{city.city}</p>
+            </div>
+            <div className="Details-p-div">
+                <div className="details-p-innerdiv">
+                <p className="Details-innerp"><span className="details-p-span">Country:</span> {city.country}.</p>
+                    <p className="Details-innerp" ><span className="details-p-span">Fundation:</span> {city.fundation}.</p>
+                    <p className="Details-innerp" ><span className="details-p-span"> Population:</span> {city.population}.</p>
+                </div>
+                <p className="Details-p" >{city.description}</p>
+                {itineraries.length? null:<h3 className="Details-subtitle"> We don't have any itineraries here right now...</h3>}
+            </div>
+                {itineraries.length? itineraries.map(itinerary => {
+                    return (
+                        <Itinerary data={itinerary} key={itinerary._id} />
+                        )
+                    }):null
+                }
+            <div className="Details-btns">
+                    <LinkRouter to={`/`} className="Details-btn">Back to Home</LinkRouter>
+                    <LinkRouter to={`/cities`}className="Details-btn">Back to Cities</LinkRouter>
+                    <LinkRouter to={`/edit-city/${id}`}className="Details-btn">Edit</LinkRouter>
+            </div>
         </div>
     )
 
