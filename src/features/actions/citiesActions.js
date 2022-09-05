7af96376437627a4bb28adc147b5b89766c03956
IbanezAndrea <mrs3lite@gmail.com>
2022-09-04 23:12:11 -0300

    Date y Reducer Get1

--- src/features/actions/citiesActions.js
+++ src/features/actions/citiesActions.js
@@ -1,15 +1,31 @@
 import axios from "axios";
 
 const citiesActions = {
-    
+    // addCity: () => {
+    //     return async () => {
+    //         const res = await axios.post(`http://localhost:4000/cities`)
+    //         dispatch({type: 'ADD_CITY', payload:  })
+    //     }
+    // },
+
     getCities: (searchValue) => {
+
             return async (dispatch,getState) => {
                 
                 const res = await axios.get(`http://localhost:4000/cities/${searchValue ? `?city=${searchValue}` : ''}` )
                 //console.log(res)
                 dispatch({type: 'ALL_CITIES', payload: res.data.response})
             }
-        }
+        },
+
+    getOneCity: (id) => {
+        
+        return async (dispatch, getState) => {
+
+            const res = await axios.get(`http://localhost:4000/cities/${id}`)
+            dispatch({type:'ONE_CITY', payload: res.data.response})
+            }
+    }
     
 
 }
