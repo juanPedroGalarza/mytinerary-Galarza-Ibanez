f9c7a4905d2cd8d641a110f1e65ffbd0ccde59ca
juanPedroGalarza <juanpg232@gmail.com>
2022-09-06 19:12:08 -0300

    Comments component

--- src/components/Itinerary/Comments.jsx
+++ src/components/Itinerary/Comments.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+import "../../styles/itinerary/Comments.css"
 export default function Comments(props) {
     const comments = [
         {
@@ -5,8 +7,7 @@ export default function Comments(props) {
             user: {
                 name: "Armando",
                 surName: "Paredes",
-                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
-                mail: "armando@paredes.xd"
+                photo: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg"
             }
         },
         {
@@ -14,18 +15,53 @@ export default function Comments(props) {
             user: {
                 name: "Richard jr",
                 surName: "Galeano",
-                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
-                mail: "craken@cuthulu.com"
+                photo: "https://i.pinimg.com/736x/c4/02/bd/c402bdca03a9e237d3368984547e2bdf.jpg"
             }
         },
         {
             message: "🐙",
             user: {
-                name: "aa",
-                surName: "Paredes",
-                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
-                mail: "armando@paredes.xd"
+                name: "Rigoberto",
+                surName: "De la Fuente",
+                photo: "https://www.tonica.la/__export/1600634330723/sites/debate/img/2020/09/20/kirito-regresarx-en-una-nueva-adaptacixn-al-anime-.jpg_1902800913.jpg"
             }
         },
     ]
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => {
+        open ?
+        setOpen(false)
+        :setOpen(true)
+    }
+    const viewComment = (comment) => {
+        return (
+            <div className="comments-item" key={comment.user.name}>
+                <div className="comments-user">
+                    <img src={comment.user.photo} alt={comment.user.name} className="comments-user-photo"/>
+                    <p>{ comment.user.name}</p>
+                    <p>{ comment.user.surName}</p>
+                    <p>{ comment.user.mail}</p>
+                </div>
+                <div className="comments-message">
+                    <p>
+                        {comment.message}
+                    </p>
+                </div>
+            </div>
+        )
+    }
+    return (
+        <>
+        <button className="comments-button" type="button" onClick={handleOpen}>
+            {open? "Close ":""}
+            Comments
+        </button>
+            {open ?
+        <div className="comments-container">
+                {comments.map(viewComment)}
+        </div>
+                :null
+            }
+        </>
+    )
 }
