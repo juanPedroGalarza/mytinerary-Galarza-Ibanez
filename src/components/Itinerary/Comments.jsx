2bd3e686a7349c80be03cceda628d6a0757e0097
juanPedroGalarza <juanpg232@gmail.com>
2022-09-06 16:30:25 -0300

    Comments component

---
+++ src/components/Itinerary/Comments.jsx
@@ -0,0 +1,31 @@
+export default function Comments(props) {
+    const comments = [
+        {
+            message: "🐙",
+            user: {
+                name: "Armando",
+                surName: "Paredes",
+                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
+                mail: "armando@paredes.xd"
+            }
+        },
+        {
+            message: "🐙",
+            user: {
+                name: "Richard jr",
+                surName: "Galeano",
+                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
+                mail: "craken@cuthulu.com"
+            }
+        },
+        {
+            message: "🐙",
+            user: {
+                name: "aa",
+                surName: "Paredes",
+                pohoto: "https://www.armandoparedes.com/static/950228c2b7910e69bb75c4688e5b8c5b/0ac58/laureles-1.jpg",
+                mail: "armando@paredes.xd"
+            }
+        },
+    ]
+}
