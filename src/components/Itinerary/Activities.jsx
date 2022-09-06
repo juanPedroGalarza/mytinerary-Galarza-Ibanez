707502fbc481833451c71f58f14d02f280d25d34
juanPedroGalarza <juanpg232@gmail.com>
2022-09-06 16:29:54 -0300

    Activities component

---
+++ src/components/Itinerary/Activities.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import "../../styles/itinerary/Activities.css"
+export default function Activities(props) {
+    const activities = [
+        {
+            name: "Act1",
+            photo: "https://imagenes.elpais.com/resizer/pKpsAzeO1aqa2M1a-6AIp_ZbxH0=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/VXSEYFTVUFDGPJH6BZFVCGH6OU.jpg",
+        },
+        {
+            name: "Act2",
+            photo: "https://imagenes.elpais.com/resizer/pKpsAzeO1aqa2M1a-6AIp_ZbxH0=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/VXSEYFTVUFDGPJH6BZFVCGH6OU.jpg",
+        },
+        {
+            name: "Act3",
+            photo: "https://imagenes.elpais.com/resizer/pKpsAzeO1aqa2M1a-6AIp_ZbxH0=/1960x1103/cloudfront-eu-central-1.images.arcpublishing.com/prisa/VXSEYFTVUFDGPJH6BZFVCGH6OU.jpg",
+        }
+    ]
+    const [open,setOpen] = useState(false)
+    const viewActivity = (activity) => {
+        return (
+            <div className="activities-item" key={activity.name} style={{ backgroundImage: `url(${activity.photo})` }}>
+                <p className="activities-item-name">{activity.name}</p>
+            </div>
+        )
+    }
+    const handleOpen=()=>{
+        open ?
+        setOpen(false)
+        :setOpen(true)
+    }
+    return (
+        <>
+            <button type="button" onClick={handleOpen} className="activities-button">
+                {open? "Close ":""}
+            Activities
+        </button>
+        <div className="activities-container">
+            {open ?
+                activities.map(viewActivity)
+            :null}
+        </div>
+        </>
+    )
+}
