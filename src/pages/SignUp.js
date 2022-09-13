4bf7fa44d141a99e57df75e3c59ab7c5a62fef19
Ibañez Andrea <108688594+IbanezAndrea@users.noreply.github.com>
2022-09-12 21:33:25 -0300

    Google button and signup page

---
+++ src/pages/SignUp.js
@@ -0,0 +1,70 @@
+import GoogleSignUp from "../components/Users/GoogleSignUp";
+import '../styles/SignUpPage.css';
+import Input from "../components/Input";
+import { useUserSignUpMutation } from "../features/actions/usersAPI";
+
+function SignUp() {
+    const inputArray =[
+        
+        {
+            name: "name",
+            type: "text",
+            placeholder: "Write your name here!",
+            value: ""
+        },
+        {
+            name: "lastName",
+            type: "text",
+            placeholder: "Write your last name here!",
+            value: ""
+        },
+        {
+            name: "email",
+            type: "email",
+            placeholder: "Write your email here",
+            value: ""
+        },
+        {
+            name: "country",
+            type: "text",
+            placeholder: "Where are you from?",
+            value: ""
+        },
+        {
+            name: "password",
+            type: "password",
+            placeholder: "Insert your password",
+            value: ""
+        },
+        {
+            name: "photo",
+            type: "url",
+            placeholder: "Insert the image url for your profile picture!",
+            value: ""
+        },
+    ]
+
+    let [userSignUp] = useUserSignUpMutation()
+    const signUserForm =(arrayform) => {
+        let data = arrayform.reduce((values,input)=>{
+            values[input.name] = input.value
+            return values
+        }, {})
+        console.log(data)
+        
+        data.role = "user"
+        data.from ="form"
+        userSignUp(data)
+    }
+
+
+    return (
+        <div className="signup-page-main">
+            <Input inputsData={inputArray}  event={signUserForm} classPage="signup" />
+            <GoogleSignUp/>
+
+        </div>
+    )
+}
+
+export default SignUp
