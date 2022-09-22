3d096f41294e9577c71d0f22458590f6855a6236
Ibañez Andrea <108688594+IbanezAndrea@users.noreply.github.com>
2022-09-22 19:40:15 -0300

    Fix Alerts

--- src/pages/NewAdmin.js
+++ src/pages/NewAdmin.js
@@ -1,43 +1,45 @@
 import { useState,useEffect, useRef } from "react"
 import { useEditUserMutation } from "../features/actions/usersAPI"
 import Alert from "../components/Alert"
-
 import "../styles/NewAdmin.css"
-import userEvent from "@testing-library/user-event"
-
 
 function NewAdmin() {
 const emailInput = useRef(null)
 const [editUser, {data: newRole, error}] = useEditUserMutation()
+const [showAlert, setShowAlert] = useState(false)
 
 
 useEffect(() => {
-
-
-}, [error])
+    if (showAlert) {
+        setTimeout(() => {
+            setShowAlert(false)
+        },5000)
+    }
+},[newRole, error])
     
 
     const inputValue = (e) => {
         e.preventDefault()
-        //console.log(emailInput.current.value)
         let email= emailInput.current.value
         let data ={
             email: email,
             role: "admin"
-        }
-
-        console.log(data)
+            }
         editUser(data)
         e.target.reset()
+        setShowAlert(true)
     }
 
     return (
         <div className="newadmin-form">
             <div className='newadmin-background'/>
             <form  onSubmit={inputValue} >
-            <input name="Email" type="email" placeholder="Insert Email of the user" ref={emailInput} className="newadmin-input"/>
+            <input name="Email" type="email" placeholder="Insert email of the user" ref={emailInput} className="newadmin-input"/>
             <button type="submit" className="newadmin-btn"> Send </button>
             </form> 
+            {showAlert ?
+                <Alert res={newRole} err={error} stop={() => setShowAlert(false)} />
+            : null}
         </div>
     )
 }
