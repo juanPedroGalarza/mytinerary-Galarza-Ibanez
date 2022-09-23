f76ece822e009e9113eecf2f6bb95244c6d41bb5
juanPedroGalarza <juanpg232@gmail.com>
2022-09-22 22:53:23 -0300

    Carousel updated

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import Arrow from "./Carousel/Arrow";
 import { useEffect, useState } from "react";
 import { Link as LinkRouter } from 'react-router-dom'
 function Carousel(props) {
-    let items = props.data.response? props.data.response : props.data
+    let items = props.data
     let range = props.range
     let [start, setStart] = useState(props.start)
     let [end, setEnd] = useState(start + range)
@@ -23,7 +23,7 @@ function Carousel(props) {
             )
         }
     function next() {
-        if (end <= items.length - range) {
+        if (end <= items?.length - range) {
             setStart(start + range)
             setEnd(end + range)
             return
